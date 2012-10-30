01472fda365a702f5d9352671ba28853ff9ec278
Benjamin Fleischer <github@benjaminfleischer.com>
2012-10-30 09:31:31 -0500

    BF getting command t to build successfully

--- install.sh
+++ install.sh
@@ -15,20 +15,7 @@ mkdir -p bundle
 git clone https://github.com/gmarik/vundle.git bundle/vundle
 vim +BundleInstall +qall
 
-# use system ruby for building plugins with ruby extensions
-# currently rvm only
-[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm" 
-echo 'switching to system ruby'
-rvm use system
-#  rbenv local system
-cd ~/.vim/bundle/Command-T/ruby/command-t
-export ARCHFLAGS="-arch i386" # I forget why we need this...
-ruby extconf.rb
-make
-echo 'done. returning to default rvm ruby'
-rvm use default
-
-cd ~/.vim
+./make_command_t.sh
 
 # install ctags
 # https://github.com/tpope/gem-ctags
