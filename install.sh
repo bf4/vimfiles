535d06942c89bc14cdd2177f871a2c6c6b4f1dcc
Benjamin Fleischer <github@benjaminfleischer.com>
2015-05-13 15:50:49 -0500

    Homeshickize

--- install.sh
+++ install.sh
@@ -2,28 +2,16 @@
 # ensure temp dirs exist
 set -e
 
-[ -e "autoload/plug.vim" ] ||
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+cwd=$(pwd)
+mkdir -p home/.vim
+cd home/.vim
 
 mkdir -p ./tmp
 mkdir -p ~/.vim-tmp
 
-# link .vim and .vimrc
-cwd=$(pwd)
-suffix=$(date +"%Y_%m_%d")
-
-from="${cwd}"
-to="$HOME/.vim"
-[ -e "$to" ] &&
-  mv "$to" "${to}.old_${suffix}"
-ln -s "$from" "$to" || $!
-
-from="${cwd}/vimrc"
-to="$HOME/.vimrc"
-[ -e "$to" ] &&
-  mv "$to" "${to}.old_${suffix}"
-ln -s "$from" "$to" || $!
+[ -e "autoload/plug.vim" ] ||
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 mkdir -p bundle
 vim +PlugClean +PlugInstall +qall
@@ -60,3 +48,6 @@ echo 'adding gem-ctags to top of rvm global gemset (hopefully not twice)'
 echo "gem-ctags" |cat - ~/.rvm/gemsets/global.gems > /tmp/out && mv /tmp/out ~/.rvm/gemsets/global.gems
 # echo "echo 'gem-ctags' >> ~/.rvm/gemsets/global.gems"
 # echo 'then Put it at the top so the gems below it will be indexed.'
+
+
+cd $pwd
